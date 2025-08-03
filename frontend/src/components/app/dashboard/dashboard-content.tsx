8250f1dc85698503731535c859879aaa44f1e248
Amir Cokoja <amircoka@gmail.com>
2025-08-03 16:35:20 +0200

    Empty dashboard

--- frontend/src/components/app/dashboard/dashboard-content.tsx
+++ frontend/src/components/app/dashboard/dashboard-content.tsx
@@ -22,24 +22,58 @@ export default function DashboardContent() {
     }
   }, [data]);
 
+  const dataAvailable = data && data.items && data.items.length > 0;
   const renderData = data ?? lastData;
 
   return (
     <>
       <TimeLogger />
       <div className="flex flex-1 flex-col justify-between">
-        <div className="mt-4 mb-10">
-          {renderData?.items?.map((group) => (
-            <div key={group.week} className="mb-8 last:mb-0">
-              <DashboardWeek weekGroup={group} />
+        {dataAvailable ? (
+          <div className="mt-4 mb-10">
+            {renderData?.items?.map((group) => (
+              <div key={group.week} className="mb-8 last:mb-0">
+                <DashboardWeek weekGroup={group} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="align-center flex h-full justify-center">
+            {/* <div className="mt-4 mb-10"> */}
+            <div className="flex h-full flex-col items-center justify-center p-6 text-center">
+              <div className="mb-4 rounded-lg bg-gray-100 p-6">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 text-gray-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </div>
+
+              <h2 className="mb-2 text-xl font-semibold text-gray-800">
+                No hours logged yet
+              </h2>
+              <p className="mb-6 max-w-md text-gray-500">
+                Start tracking your working hours to see your time in calendar.
+              </p>
             </div>
-          ))}
-        </div>
-        <Pagination
-          currentPage={page}
-          totalPages={renderData?.totalPages || 1}
-          onPageChange={setPage}
-        />
+          </div>
+        )}
+        {dataAvailable && (
+          <Pagination
+            currentPage={page}
+            totalPages={renderData?.totalPages || 1}
+            onPageChange={setPage}
+          />
+        )}
       </div>
     </>
   );
