a641840ba1b62f98c0d285ef5e587f6ebfb21c51
Amir Cokoja <amircoka@gmail.com>
2025-08-07 23:36:49 +0200

    Fav icon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <ThemeModeScript />
       </head>
       <body className={inter.className}>
